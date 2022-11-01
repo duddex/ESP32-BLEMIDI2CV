2a3db3c0d6059e5e1e0ccc2f724cb8c213ba1b2b
Stefan Dudde <stefan.dudde@gmail.com>
2022-11-01 10:17:16 +0100

    Created version with two MIDI channels
    TODO: test pitch bend implementation

--- ESP32-BLEMIDI2CV.ino
+++ ESP32-BLEMIDI2CV.ino
@@ -5,68 +5,102 @@
 const int LED_BUILTIN = 2;
 const int TRIGGER_PIN1 = 32;
 const int TRIGGER_PIN2 = 33;
-const int POT_PIN = 34;
 const int CV_PIN1 = 25;
 const int CV_PIN2 = 26;
 const int MAX_OUTPUT_VOLTAGE = 3300;
-const int NUMBER_OF_MODES = 2;
 const int LOWNOTE = 48;
 const int HIGHNOTE = LOWNOTE + 36; // 3 octaves 
 
 // global variables
-int potvalue, oldpotvalue;
-int modevalue, oldmodevalue;
-int pitchbendrange = 2; // range in semitones -> TODO: should be selectable via potentiometer (like "mode")
-float pitchvoltage = 0, pitchbendvoltage = 0;
+int pitchbendrange = 2; // range in semitones -> TODO: should be selectable for example via potentiometer
+float pitchvoltage1 = 0, pitchbendvoltage1 = 0, pitchvoltage2 = 0, pitchbendvoltage2 = 0;
 
 // ---------------------------------------------------------------------------
 void onNoteOn(uint8_t channel, uint8_t note, uint8_t velocity, uint16_t timestamp) {
+  if (velocity == 0) {
+    onNoteOff(channel, note, velocity, timestamp);
+  }
+
   if (note<LOWNOTE) note=LOWNOTE;
   if (note>HIGHNOTE) note=HIGHNOTE;
 
-  pitchvoltage = (((note-LOWNOTE)/12.0)*1000.0);
-  if (pitchvoltage + pitchbendvoltage < 0) {
-    dacWrite(CV_PIN1, 0);
-  } else {
-    dacWrite(CV_PIN1, map(pitchvoltage + pitchbendvoltage, 0, MAX_OUTPUT_VOLTAGE, 0, 255));
-  }
+  switch (channel) {
+    case 0: // MIDI channel 1
+      digitalWrite(TRIGGER_PIN1, HIGH);
+      pitchvoltage1 = (((note-LOWNOTE)/12.0)*1000.0);
+      if (pitchvoltage1 + pitchbendvoltage1 < 0) {
+        dacWrite(CV_PIN1, 0);
+      } else {
+        dacWrite(CV_PIN1, map(pitchvoltage1 + pitchbendvoltage1, 0, MAX_OUTPUT_VOLTAGE, 0, 255));
+      }
+      //Serial.printf("Note on: MIDI channel %d, note %d (%fmV + %fmV), velocity %d\n", channel+1, note, pitchvoltage1, pitchbendvoltage1, velocity);
+      break;
 
-  if (modevalue == 1) { // mode 1: CV_PIN2 is used to send velocity value
-    dacWrite(CV_PIN2, map(velocity, 0, 127, 0, 255));
+    case 1: // MIDI channel 2
+      digitalWrite(TRIGGER_PIN2, HIGH);
+      pitchvoltage2 = (((note-LOWNOTE)/12.0)*1000.0);
+      if (pitchvoltage2 + pitchbendvoltage2 < 0) {
+        dacWrite(CV_PIN2, 0);
+      } else {
+        dacWrite(CV_PIN2, map(pitchvoltage2 + pitchbendvoltage2, 0, MAX_OUTPUT_VOLTAGE, 0, 255));
+      }
+      //Serial.printf("Note on: MIDI channel %d, note %d (%fmV + %fmV), velocity %d\n", channel+1, note, pitchvoltage2, pitchbendvoltage2, velocity);
+      break;
+    
+    default:
+      break;
   }
-
-  Serial.printf("Note on: MIDI channel %d, note %d (%fmV + %fmV), velocity %d\n", channel+1, note, pitchvoltage, pitchbendvoltage, velocity);
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(TRIGGER_PIN1, HIGH);
 }
 
 // ---------------------------------------------------------------------------
 void onNoteOff(uint8_t channel, uint8_t note, uint8_t velocity, uint16_t timestamp) {
-  Serial.printf("Note off: channel %d, note %d, velocity\n", channel+1, note, velocity);
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(TRIGGER_PIN1, LOW);
-}
+  //Serial.printf("Note off: channel %d, note %d\n", channel+1, note);
+  switch (channel) {
+    case 0: // MIDI channel 1
+      digitalWrite(TRIGGER_PIN1, LOW);
+      break;
 
-// ---------------------------------------------------------------------------
-void onControlChange(uint8_t channel, uint8_t controller, uint8_t value, uint16_t timestamp) {
-  // controller 1: modwheel
-  // mode 2: CV_PIN2 is used to send modwheel value
-  if ((controller == 1) && (modevalue == 2)) { 
-    Serial.printf("Modwheel change: MIDI channel %d, value %d\n", channel+1, value);
-    dacWrite(CV_PIN2, map(value, 0, 127, 0, 255));
+    case 1: // MIDI channel 2
+      digitalWrite(TRIGGER_PIN2, LOW);
+      break;
+    
+    default:
+      break;
   }
 }
 
 // ---------------------------------------------------------------------------
+// TODO: not tested yet
 void onPitchbend(uint8_t channel, uint16_t value, uint16_t timestamp) {
-  // The range of value here is 0 to 16383. Pitch bend wheel in "neutral position" is value 8192
-  pitchbendvoltage = (map(value, 0, 16383, pitchbendrange * -1 * 1000, pitchbendrange * 1000) * (1000.0 / 12.0)) / 1000;
-  Serial.printf("Pitch bend: MIDI channel %d, value %d, pitchvoltage %fmV, pitchbendvoltage %fmV\n", channel+1, value, pitchvoltage, pitchbendvoltage);
-  if (pitchvoltage + pitchbendvoltage < 0) {
-    dacWrite(CV_PIN1, 0);
-  } else {
-    dacWrite(CV_PIN1, map(pitchvoltage + pitchbendvoltage, 0, MAX_OUTPUT_VOLTAGE, 0, 255));
+  switch (channel) {
+    case 0: // MIDI channel 1
+      // The range of value here is 0 to 16383. Pitch bend wheel in "neutral position" is value 8192
+      pitchbendvoltage1 = (map(value, 0, 16383, pitchbendrange * -1 * 1000, pitchbendrange * 1000) * (1000.0 / 12.0)) / 1000;
+  
+      if (pitchvoltage1 + pitchbendvoltage1 < 0) {
+        dacWrite(CV_PIN1, 0);
+      } else {
+        dacWrite(CV_PIN1, map(pitchvoltage1 + pitchbendvoltage1, 0, MAX_OUTPUT_VOLTAGE, 0, 255));
+      }
+      //Serial.printf("Pitch bend: MIDI channel %d, value %d, pitchvoltage %fmV, pitchbendvoltage %fmV\n", channel+1, value, pitchvoltage1, pitchbendvoltage1);
+      break;
+
+      case 1: // MIDI channel 2
+      // The range of value here is 0 to 16383. Pitch bend wheel in "neutral position" is value 8192
+      pitchbendvoltage2 = (map(value, 0, 16383, pitchbendrange * -1 * 1000, pitchbendrange * 1000) * (1000.0 / 12.0)) / 1000;
+  
+      if (pitchvoltage2 + pitchbendvoltage2 < 0) {
+        dacWrite(CV_PIN2, 0);
+      } else {
+        dacWrite(CV_PIN2, map(pitchvoltage2 + pitchbendvoltage2, 0, MAX_OUTPUT_VOLTAGE, 0, 255));
+      }
+      //Serial.printf("Pitch bend: MIDI channel %d, value %d, pitchvoltage %fmV, pitchbendvoltage %fmV\n", channel+1, value, pitchvoltage2, pitchbendvoltage2);
+      break;
+
+    default:
+      break;
   }
+  
 }
 
 // ---------------------------------------------------------------------------
@@ -82,26 +116,22 @@ void blink(int number, int highdelay, int lowdelay) {
 // ---------------------------------------------------------------------------
 void setup() {
   Serial.begin(115200);
-  Serial.println("ESP32 BLE MIDI device started");
+  //Serial.println("ESP32 BLE MIDI device started");
   BLEMidiServer.begin("ESP32 BLE MIDI device");
 
   BLEMidiServer.setOnConnectCallback([]() {
-    Serial.println("Connected");
+    //Serial.println("Connected");
     blink(10, 25, 25);
   });
 
   BLEMidiServer.setNoteOnCallback(onNoteOn);
   BLEMidiServer.setNoteOffCallback(onNoteOff);
-  BLEMidiServer.setControlChangeCallback(onControlChange);
-  BLEMidiServer.setPitchBendCallback(onPitchbend);
+  //BLEMidiServer.setPitchBendCallback(onPitchbend);
 
   // There are 2 x 8 bits DAC channels on the ESP32 to convert digital signals into
   // analog voltage signal outputs. These are the DAC channels: DAC1 (GPIO25), DAC2 (GPIO26)
   pinMode(CV_PIN1, ANALOG);
   pinMode(CV_PIN2, ANALOG);
-
-  // Potentiometer for changing the mode
-  pinMode(POT_PIN, INPUT);
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(TRIGGER_PIN1, OUTPUT);
@@ -113,14 +143,5 @@ void setup() {
 
 // ---------------------------------------------------------------------------
 void loop() {
-  potvalue = analogRead(POT_PIN);
-  if (oldpotvalue != potvalue) {
-    oldpotvalue = potvalue;
-    modevalue = map(potvalue, 0, 4095, 1, NUMBER_OF_MODES);
-    if (oldmodevalue != modevalue) {
-      Serial.printf("Mode change: %d\n", modevalue);
-      blink(modevalue, 200, 100);
-      oldmodevalue = modevalue;
-    }    
-  }
+  // nothing to do here :-)
 }
--- README.md
+++ README.md
@@ -2,22 +2,4 @@
 
 This is a module for converting MIDI messages to control voltage (CV) with an ESP32 using the ESP32-BLE-MIDI library from Maxime ANDRÉ (https://github.com/max22-/ESP32-BLE-MIDI).
 
-There are 2 x 8 bits DAC channels on the ESP32 to convert digital signals into analog voltage signal outputs. These are the DAC channels: DAC1 (GPIO25), DAC2 (GPIO26). The MIDI data is received via Bluetooth and will be converted into control voltage for controlling analog synthesizers.
-
-Note on and note off information is sent to PIN32 and PIN33. Note information, velocity information and modwheel information is converted to CV and sent to PIN25 and PIN26.
-
-At the moment the ESP32-BLEMIDI2CV module supports two modes which can be selected via potentiometer connected to PIN 34.
-
-## Mode 1
-* Note on/Note off is sent to PIN32 (Note on: HIGH, Note off, LOW)
-* Note information is converted to control voltage and sent to PIN25
-* Velocity information is converted to control voltage and sent to PIN26
-
-## Mode 2
-* Note on/Note off is sent to PIN32 (Note on: HIGH, Note off, LOW)
-* Note information is converted to control voltage and sent to PIN25
-* Modwheel information is converted to control voltage and sent to PIN26
-
-Turn the potentiometer to the left for mode 1. Turn it to the right for mode 2.
-
-In mode 1 and mode 2 the module listens to all MIDI channels.
+There are 2 x 8 bits DAC channels on the ESP32 to convert digital signals into analog voltage signal outputs. These are the DAC channels: DAC1 (GPIO25), DAC2 (GPIO26). The MIDI data is received via Bluetooth and will be converted into control voltage for controlling analog synthesizers. Note on and note off information is sent to PIN32 (MIDI channel 1) and PIN33 (MIDI channel 2). Note information is converted to CV and sent to PIN25 (MIDI channel 1) and PIN26 (MIDI channel 2).
